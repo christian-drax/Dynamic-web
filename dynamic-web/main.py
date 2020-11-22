b916b504ca0ead45b652a264956d3277caa07e36
christian-drax <73065554+christian-drax@users.noreply.github.com>
2020-11-22 20:35:15 +0100

    git commit -m"myfirstcommit"

--- dynamic-web/main.py
+++ dynamic-web/main.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import models
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bookexampledbpassword@localhost:5432/bookexample'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -20,3 +28,32 @@ def about_us():
 
 if __name__ == "__main__":
     app.run(port=5001) # here we are using a different port so as not to conflict with that allocated to our helloworld.py
+
+
+@app.route("/signup/")
+def signup():
+    return render_template('signup.html', title="SIGN UP", information="Use the form displayed to register")
+
+
+@app.route("/process-signup/", methods=['POST'])
+def process_signup():
+    # Let's get the request object and extract the parameters sent into local variables.
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    othernames = request.form['othernames']
+    email = request.form['email']
+    password = request.form['password']
+    # let's write to the database
+    try:
+      user = models.User(firstname=firstname, lastname=lastname, othernames=othernames, email=email, password=password)
+      db.session.add(user)
+      db.session.commit()
+    except Exception as e:
+       # Error caught, prepare error information for return
+       information = 'Could not submit. The error message is {}'.format(e.__cause__)
+       return render_template('signup.html', title="SIGN-UP", information=information)
+
+    # If we have gotten to this point, it means that database write has been successful. Let us compose success info
+    information = 'User by name {} {} successfully added. The login name is the email address {}.'.format(firstname, lastname, email)
+
+    return render_template('signup.html', title="SIGN-UP", information=information)
